cecde01be86d52bb7d2bd1ecf5971f2d6cff8364
Ahnaf Arif <2080arif@gmail.com>
2021-07-01 10:00:25 +0600

    all added

---
+++ null-undesined.js
@@ -0,0 +1,22 @@
+//undefined 
+let name;
+function add (num1, num2) {
+    console.log(num1 + num2);
+    return                  // return undefined
+}
+
+function add (num1, num2) {
+    
+    return num1 + num2
+}
+console.log(add(num1)) // num2 undefined
+
+const person = {name: "Hosen", age: 25, job: "student" };
+console.log(person.friend); // friend undefined
+
+let fun = undefined;
+console.log(fun); 
+
+//nul
+
+
