3f02d1489d36557c5f7746f19b8258ea16dcc623
Ahnaf Arif <2080arif@gmail.com>
2021-07-01 09:14:17 +0600

    all added

--- modern-javascript.js
+++ modern-javascript.js
@@ -1,1 +1,20 @@
-console.log(5)
+// falsy values
+// false
+// 0
+// ""
+// undefined 
+// null
+//NaN 
+
+// Truthy values
+// "0"
+// ' '
+// empty array []
+// empty object {}
+let name = false;
+if (name) {
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
