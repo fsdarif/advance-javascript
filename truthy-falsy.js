9b46d129ea45c68fb1a083de356233c4ad66b77f
Ahnaf Arif <2080arif@gmail.com>
2021-07-01 09:15:47 +0600

    all added

---
+++ truthy-falsy.js
@@ -0,0 +1,20 @@
+// falsy values
+// false
+// 0
+// ""
+// undefined 
+// null
+//NaN 
+
+// Truthy values
+// "0"
+// ' '
+// empty array []
+// empty object {}
+let name = false;
+if (name) {
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
